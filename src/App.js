0547f7bdd74c124bf3afd7128e85b22d3d3cf673
austin andersen <austinjamesgf@gmail.com>
2022-03-24 18:33:13 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+ 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+ 
+ 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        console.log(res.data)
+        setCharacters(res.data);
+        res.data.map(fr => {
+          return fr;
+        })
+      })
+      .catch(err => console.error(err))
+  }, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+ 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+ 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS</h1>
+      { characters.map(fr => {
+        return <Character info={fr} key={fr.id}/>
+      })
+    }
     </div>
   );
 }
-
+ 
 export default App;
+ 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,22 @@
+
+import React from 'react';
+import styled from 'styled-components';
+ 
+ 
+const StyledCharacter = styled.div`
+    background-color: #FAEBD7;
+    font-size: 17px;
+    padding: 5%;
+    border-style: solid;
+    border-color: solid red;
+`
 // Write your Character component here
+ 
+ 
+export default function Character(props){
+    return (
+        <StyledCharacter className='character-container'>
+            {props.info.name} {props.info.birth_year}
+        </StyledCharacter>
+    )
+};
